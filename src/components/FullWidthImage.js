8f936004c94aa7dab9c6600253dc5e09a8cb59ea
Ken Fehling <me@kenfehling.com>
2022-04-20 05:23:59 -0400

    Try without avif

--- src/components/FullWidthImage.js
+++ src/components/FullWidthImage.js
@@ -39,7 +39,7 @@ export default function FullWidthImage(props) {
             aspectratio={3 / 1}
             // This is a presentational image, so the alt should be an empty string
             alt=""
-            formats={["auto", "webp", "avif"]}
+            formats={["auto", "webp"]}
           />
         ) : (
           <GatsbyImage
@@ -57,7 +57,7 @@ export default function FullWidthImage(props) {
             aspectratio={3 / 1}
             // This is a presentational image, so the alt should be an empty string
             alt=""
-            formats={["auto", "webp", "avif"]}
+            formats={["auto", "webp"]}
           />
         )}
         {(title || subheading) && (
