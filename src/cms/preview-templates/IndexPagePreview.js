baad244c52851e66d4240f0950fddb02aeed2e9f
Ken Fehling <me@kenfehling.com>
2022-04-20 18:39:44 -0400

    Theme

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { IndexPageTemplate } from '../../templates/index-page'
+import getTheme from '../../theme';
 import { ThemeProvider } from '@mui/system'
 
 const IndexPagePreview = ({ entry, getAsset }) => {
